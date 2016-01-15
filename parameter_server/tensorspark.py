fa32340305ac92e4e4f4ee219e7fb1215dabe9cd
csmith <csmith@adatao.com>
2016-01-14 16:03:25 -0800

    Added epoch functionality, made training a little more generic, made minibatch size variable

--- parameter_server/tensorspark.py
+++ parameter_server/tensorspark.py
@@ -8,12 +8,13 @@ def train_partition(partition):
 def test_partition(partition):
 	return parameterwebsocketclient.TensorSparkWorker().test_partition(partition)
 
-
 # you can find the mnist csv files here http://pjreddie.com/projects/mnist-in-csv/
-def train_all():
-	training_rdd = sc.textFile('./mnist_train.csv')
-	mapped_training = training_rdd.mapPartitions(train_partition)
-	mapped_training.collect()
+def train_epochs(num_epochs, batch_size):
+	training_rdd = sc.textFile('/Users/christophersmith/code/adatao/tensorspark/data/medium_mnist_train.csv')
+	for i in range(num_epochs):
+		mapped_training = training_rdd.mapPartitions(train_partition)
+		mapped_training.collect()
+		training_rdd.repartition(training_rdd.count()/batch_size)
 
 def test_all():
 	testing_rdd = sc.textFile('./mnist_test.csv')
@@ -21,5 +22,9 @@ def test_all():
 	return mapped_testing.reduce(add)/testing_rdd.count()
 
 sc = pyspark.SparkContext()
-train_all()
+
+num_epochs = 3
+batch_size = 100
+train_epochs(num_epochs, batch_size)
+
 print test_all()
