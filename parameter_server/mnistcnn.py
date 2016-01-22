Fixed some bugs. Wa not evaluating assignment of parameters. Switched to more numerically stable cross_entropy for mnistcnn. Also added warmup routine

parameter_server/mnistcnn.py
@@ -37,15 +37,19 @@ class MnistCNN(ParameterServerModel):
       b_fc1 = bias_variable([1024], 'b_fc1')
       h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
       h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-      self.keep_prob = tf.placeholder("float", name='keep_prob')
-      h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
+      keep_prob = tf.placeholder("float", name='keep_prob')
+      h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
       W_fc2 = weight_variable([1024, 10], 'W_fc2')
       b_fc2 = bias_variable([10], 'b_fc2')
-      y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+#      y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+      y_conv=tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
       variables = [W_conv1, b_conv1, W_conv2, b_conv2, W_fc1, b_fc1, W_fc2, b_fc2]
-      loss = -tf.reduce_sum(y_ * tf.log(y_conv))
-      optimizer = tf.train.AdamOptimizer(1e-4)
+#      loss = -tf.reduce_sum(y_ * tf.log(y_conv))
+      loss = tf.nn.softmax_cross_entropy_with_logits(y_conv, y_)
+
+      optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
+      minimize = optimizer.minimize(loss)
       correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
       accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
       compute_gradients = optimizer.compute_gradients(loss, variables)
@@ -55,5 +59,5 @@ class MnistCNN(ParameterServerModel):
          #name = 'placeholder_%s' % grad_var[1].name
          placeholder_gradients.append((tf.placeholder('float', shape=grad_var[1].get_shape()) ,grad_var[1]))
       apply_gradients = optimizer.apply_gradients(placeholder_gradients)
-      ParameterServerModel.__init__(self, x, y_, compute_gradients, apply_gradients, accuracy, session, placeholder_gradients)
+      ParameterServerModel.__init__(self, x, y_, keep_prob, compute_gradients, apply_gradients, minimize, accuracy, session, placeholder_gradients)
 

parameter_server/parameterservermodel.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ParameterServerModel():
 
-   def __init__(self, x, y_, compute_gradients, apply_gradients, accuracy, session, placeholder_gradients):
+   def __init__(self, x, y_, keep_prob, compute_gradients, apply_gradients, minimize, accuracy, session, placeholder_gradients):
       if type(x) != tf.python.framework.ops.Tensor:
          raise(TypeError('x must be of type tf.python.framework.ops.Tensor'))
       if type(y_) != tf.python.framework.ops.Tensor:
@@ -11,7 +11,7 @@ class ParameterServerModel():
       if type(accuracy) != tf.python.framework.ops.Tensor:
          raise(TypeError('accuracy must be of type tf.python.framework.ops.Tensor'))
       if self.compute_gradients_correct_type(compute_gradients) == False:
-         raise(TypeError('compute_gradients must be a list of tuples of type (tf.python.framework.ops.Tensor,tf.python.ops.variables.Variable)'))
+         raise(TypeError('compute_gradients must be a list of tuples of type (tf.python.framework.ops.Tensor,tf.python.ops.Variable)'))
       if type(apply_gradients) != tf.python.framework.ops.Operation:
          raise(TypeError('apply_gradients must be of type tf.python.framework.ops.Operation'))
 #      if type(optimizer) != tf.python.training.adam.AdamOptimizer:
@@ -24,12 +24,17 @@ class ParameterServerModel():
       self.session.graph.as_default().__enter__()
       self.x = x
       self.y_ = y_
+      self.keep_prob = keep_prob
       self.compute_gradients = compute_gradients
       self.apply_gradients = apply_gradients
       self.accuracy = accuracy
+      self.accuracy_summary = tf.scalar_summary("accuracy", accuracy)
+      self.minimize = minimize
       self.reset_gradients()
       self.gradient_counter = tf.Variable(initial_value=0, trainable=False)
       self.placeholder_gradients = placeholder_gradients
+      self.merged = tf.merge_all_summaries()
+      self.writer = tf.train.SummaryWriter("./logs", self.session.graph_def)
       self.session.run(tf.initialize_all_variables())
 
    def compute_gradients_correct_type(self, compute_gradients):
@@ -38,20 +43,25 @@ class ParameterServerModel():
       for element in compute_gradients:
          if type(element) != tuple or len(element) != 2:
             return False
-         if type(element[0]) != tf.python.framework.ops.Tensor or type(element[1]) != tf.python.ops.tensorflow.variables.Variable:
+         if type(element[0]) != tf.python.framework.ops.Tensor or type(element[1]) != tf.python.ops.tensorflow.Variable:
             return False
       return True
-
 
    def get_num_classes(self):
       return self.y_.get_shape().as_list()[1]
 
    def train(self, labels, features):
       feed_dict={self.x: features, self.y_: labels, self.keep_prob: 0.5}
-      self.num_gradients += 1
       # this can probably be made more efficiently with tf.gradients or tf.add
       self.gradients = np.add(self.gradients, [grad_var[0].eval(feed_dict=feed_dict) for grad_var in self.compute_gradients])
-      return self.accuracy.eval(feed_dict=feed_dict)
+      #summary = self.merged.eval(feed_dict=feed_dict)
+      accuracy = self.accuracy.eval(feed_dict=feed_dict)
+#      summary, accuracy = self.session.run([self.merged, self.accuracy], feed_dict=feed_dict)
+      #accuracy = self.accuracy.eval(feed_dict=feed_dict)
+#      self.writer.add_summary(summary, self.num_gradients)
+
+      self.num_gradients += 1
+      return accuracy
 
    def test(self, labels, features):
       feed_dict = {self.x: features, self.y_: labels, self.keep_prob: 1.0}
@@ -67,7 +77,7 @@ class ParameterServerModel():
       for i, grad_var in enumerate(self.compute_gradients):
          variable = grad_var[1]
          parameter = parameters[i]
-         variable.assign(parameters[i])
+         variable.assign(parameters[i]).eval()
 
    def apply(self, gradients):
       with self.graph.as_default():
@@ -83,7 +93,7 @@ class ParameterServerModel():
 #         for i, grad_var in enumerate(self.compute_gradients):
 #            feed_dict[grad_var[0]] = grads_and_vars[i][0]
 #         feed_dict = { self.compute_gradients: grads_and_vars}
-         print 'applying gradients %s' % grads_and_vars
+#         print 'applying gradients %s' % grads_and_vars
          #apply_gradients = self.optimizer.apply_gradients(grads_and_vars) #, global_step=self.gradient_counter)
          #print 'graph for apply_gradients'
          #for op in apply_gradients.graph.get_operations():
@@ -102,3 +112,74 @@ class ParameterServerModel():
       self.num_gradients = 0
 
 
+   def train_warmup(self, partition, batch_size=100): 
+      accuracies = []
+      iteration = 0
+      while True:
+         labels, features = process_warmup_data(self, partition, batch_size)
+
+         if len(labels) is 0:
+            break
+         with self.session.as_default():
+            #accuracy = self.train(labels, features)
+            feed = {self.x: features, self.y_: labels, self.keep_prob: 0.5}
+            self.minimize.run(feed_dict = feed)
+            accuracy = self.accuracy.eval(feed_dict={self.x: features, self.y_: labels, self.keep_prob: 1.0})
+            accuracies.append(accuracy)
+            iteration += 1
+            print 'Warmup training iteration %d at %f accuracy' % (iteration, accuracy)
+
+      return accuracies
+
+# pulled this out of the class so we can do static vars via attaching properties to the function
+def process_warmup_data(model, partition, batch_size=0):
+   if 'next_index' not in process_warmup_data.__dict__:
+      process_warmup_data.next_index = 0
+   num_classes = model.get_num_classes()
+   features = []
+   labels = []
+   if batch_size == 0:
+      batch_size = len(partition)
+   last_index = min(len(partition), process_warmup_data.next_index + batch_size)
+   for i in range(process_warmup_data.next_index, last_index):
+      line = partition[i]
+      if len(line) is 0:
+         print 'Skipping empty line'
+         continue
+      label = [0] * num_classes
+      split = line.split(',')
+      split[0] = int(split[0])
+      if split[0] >= num_classes:
+         print 'Error label out of range: %d' % split[0]
+         continue
+      features.append(split[1:])
+      label[split[0]] = 1
+      labels.append(label)
+
+   process_warmup_data.next_index = last_index
+   return labels, features
+
+def xavier_init(n_inputs, n_outputs, uniform=True):
+  """Set the parameter initialization using the method described.
+  This method is designed to keep the scale of the gradients roughly the same
+  in all layers.
+  Xavier Glorot and Yoshua Bengio (2010):
+           Understanding the difficulty of training deep feedforward neural
+           networks. International conference on artificial intelligence and
+           statistics.
+  Args:
+    n_inputs: The number of input nodes into each output.
+    n_outputs: The number of output nodes for each input.
+    uniform: If true use a uniform distribution, otherwise use a normal.
+  Returns:
+    An initializer.
+  """
+  if uniform:
+    # 6 was used in the paper.
+    init_range = math.sqrt(6.0 / (n_inputs + n_outputs))
+    return tf.random_uniform_initializer(-init_range, init_range)
+  else:
+    # 3 gives us approximately the same limits as above since this repicks
+    # values greater than 2 standard deviations from the mean.
+    stddev = math.sqrt(3.0 / (n_inputs + n_outputs))
+    return tf.truncated_normal_initializer(stddev=stddev)

parameter_server/tensorspark.py
@@ -46,25 +46,27 @@ class ParameterServer(threading.Thread):
 				model_path = message['model_path']
 				ParameterServer.saver.restore(ParameterServer.model.session, model_path)
 			else:
-				print 'Unknown message type %s' % message['type']	
+				print 'Unknown message type %s' % message['type']
 
 	model = mnistcnn.MnistCNN()
 	saver = tf.train.Saver()
 	lock = threading.Lock()
 	application = tornado.web.Application([(r"/", ParameterServerWebsocketHandler)])
 
-	def __init__(self, warmup_data):
+	def __init__(self, warmup_data=None):
 		threading.Thread.__init__(self)
 		self.warmup_data = warmup_data
+		self.warmup(warmup_data)
+
 	def warmup(self, data=None):
-		ParameterServer.model.train_partition(data)
+		if data is not None:
+			ParameterServer.model.train_warmup(data)
 
 	def run(self):
-		self.warmup()
 		self.application.listen(55555)
-		print 'listening at port 55555' 
-  		tornado.ioloop.IOLoop.current().start()
- 	
+		print 'listening at port 55555'
+   		tornado.ioloop.IOLoop.current().start()
+
 
 def train_partition(partition):
 	return parameterwebsocketclient.TensorSparkWorker().train_partition(partition)
@@ -73,12 +75,11 @@ def test_partition(partition):
 	return parameterwebsocketclient.TensorSparkWorker().test_partition(partition)
 
 # you can find the mnist csv files here http://pjreddie.com/projects/mnist-in-csv/
-def train_epochs(num_epochs):
-	training_rdd = sc.textFile('/Users/christophersmith/code/adatao/tensorspark/data/mnist_train.csv')
-#	training_rdd = sc.textFile('/Users/christophersmith/code/adatao/tensorspark/data/medium_mnist_train.csv')
+def train_epochs(num_epochs, training_rdd):
 	for i in range(num_epochs):
 		mapped_training = training_rdd.mapPartitions(train_partition)
-		mapped_training.collect()
+		accuracies = mapped_training.collect()
+		print 'accuracies: %s' % accuracies
 		training_rdd.repartition(training_rdd.getNumPartitions())
 
 def test_all():
@@ -90,19 +91,25 @@ def save_model():
 	message = {'type': 'save_model'}
 	websock.send(json.dumps(message))
 
-def start_parameter_server():
-	parameter_server = ParameterServer()
+def start_parameter_server(warmup_data):
+	parameter_server = ParameterServer(warmup_data=warmup_data)
 	parameter_server.start()
 	return parameter_server
 
-parameter_server = start_parameter_server()
-raw_input('Press enter to continue\n')
 try:
 	sc = pyspark.SparkContext()
-	websock = websocket.create_connection('ws://localhost:55555')
+	warmup_iterations = 2000
+#	training_rdd = sc.textFile('/Users/christophersmith/code/adatao/tensorspark/data/mnist_train.csv')
+	training_rdd = sc.textFile('/Users/christophersmith/code/adatao/tensorspark/data/medium_mnist_train.csv')
+	warmup_data = training_rdd.take(warmup_iterations)
+	parameter_server = start_parameter_server(warmup_data)
+	raw_input('Press enter to continue\n')
+
 	num_epochs = 3
-	train_epochs(num_epochs)
+	training_rdd = training_rdd.subtract(sc.parallelize(warmup_data))
+	train_epochs(num_epochs, training_rdd)
 	print 'Done training'
+	websock = websocket.create_connection('ws://localhost:55555')
 	save_model()
 	print 'Testing now'
 	print test_all()