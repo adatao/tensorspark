5a023b7c9ef063afc2ca72c826d54d6dbbcfbfe1
root <root@ip-172-31-3-244.ec2.internal>
2016-02-01 03:09:24 +0000

    Lots of changes. Refactored to have 1 line change to change model. Changed serialization method to numpy.savez_compressed, refactored so everyone calls a de/serialize method instead of using their own. Refactored so batch_size is in one place and part of config. Same for num_partitions, which is now being used correctly.

--- parameter_server/parameterwebsocketclient.py
+++ parameter_server/parameterwebsocketclient.py
@@ -1,21 +1,17 @@
 #!/usr/local/bin/bpython
 # -*- coding: utf-8 -*-
-#import websocket
 import json
 import tensorflow as tf
-#from tensorsparkmodel import TensorSparkModel
-#from mnistcnn import MnistCNN
 import mnistdnn
 import higgsdnn
 import moleculardnn
-#import higgsdnn
-#import download_mnist
-import pickle
+#import pickle
 import math
+import numpy as np
 import tornado.websocket
 from tornado import gen 
 from tornado.ioloop import IOLoop
-
+import cStringIO
 # TODO
 # Imagenet
 # Tachyon
@@ -23,49 +19,53 @@ from tornado.ioloop import IOLoop
 
 class TensorSparkWorker():
 
-   def __init__(self):
-      self.model = mnistdnn.MnistDNN()      
-#      self.model = moleculardnn.MolecularDNN()
-#      self.websock = websocket.create_connection('ws://localhost:55555')
+   def __init__(self, model_keyword, batch_size, websocket_port):
+      self.batch_size = batch_size
+      self.websocket_port = websocket_port
+      if model_keyword == 'mnist':
+          self.model = mnistdnn.MnistDNN(batch_size)    
+      elif model_keyword == 'higgs':
+          self.model = higgsdnn.HiggsDNN(batch_size)  
+      elif model_keyword == 'molecular':
+          self.model = moleculardnn.MolecularDNN(batch_size)
+      else:
+	  raise
       IOLoop.current().run_sync(self.init_websocket)
       self.iteration = 0
 
    @gen.coroutine
    def init_websocket(self):
-      self.websock = yield tornado.websocket.websocket_connect("ws://localhost:55555/")
+      self.websock = yield tornado.websocket.websocket_connect("ws://172.31.3.244:%d/" % self.websocket_port)
 
    def train_partition(self, partition): 
-      batch_size = 100
-      accuracies = []
       while True:
-         print 'TensorSparkWorker().train_partition iteration %d' % self.iteration
-         labels, features = self.model.process_partition(partition, batch_size)
+         #print 'TensorSparkWorker().train_partition iteration %d' % self.iteration
+         labels, features = self.model.process_partition(partition)
 
          if len(labels) is 0:
             break
 
          if self.time_to_pull(self.iteration):
-            self.request_parameters()
+		self.request_parameters()
 
-         accuracy = self.model.train(labels, features)
-         accuracies.append(accuracy)
+         self.model.train(labels, features)
          self.iteration += 1
 
          if self.time_to_push(self.iteration):
             self.push_gradients()
 
-      return accuracies
+      return []
       #return [self.train(x) for x in partition]
 
    def test_partition(self, partition):
       labels, features = self.model.process_partition(partition)
       self.request_parameters()
-      accuracy = self.model.test(labels, features)
-      return [accuracy]      
+      error_rate = self.model.test(labels, features)
+      return [error_rate]      
       #return [self.test(x) for x in partition]
 
    def test(self, data):
-      print 'TensorSparkWorker().test "%s"' % data
+      #print 'TensorSparkWorker().test "%s"' % data
       if len(data) is 0:
          return 1.0
       self.request_parameters()
@@ -88,10 +88,11 @@ class TensorSparkWorker():
    def request_parameters_coroutine(self):
       request_model_message = {'type':'client_requests_parameters'}
       self.websock.write_message(json.dumps(request_model_message))
-      print 'requesting parameters'
-      pickled_parameters = yield self.websock.read_message()
-      parameters = pickle.loads(pickled_parameters)
-      print 'received parameters'
+      #print 'requesting parameters'
+      parameters = yield self.websock.read_message()
+      parameters = self.model.deserialize(parameters)
+      #parameters = pickle.loads(pickled_parameters)
+      #print 'received parameters'
       self.model.assign_parameters(parameters)
 
    def push_gradients(self):
@@ -99,9 +100,9 @@ class TensorSparkWorker():
 
    @gen.coroutine
    def push_gradients_coroutine(self):
-      print 'pushing gradients'
-      gradient = pickle.dumps(self.model.get_gradients())
-      gradient_update_message = {'type':'client_gives_gradient', 'gradient':gradient}
-      self.websock.write_message(json.dumps(gradient_update_message))
-      print 'pushed gradients'      
+      	#print 'pushing gradients'
+      	gradients = self.model.serialize(self.model.get_gradients())
+	gradient_update_message = {'type':'client_gives_gradient', 'gradient':gradients}
+      	self.websock.write_message(json.dumps(gradient_update_message))
+      	#print 'pushed gradients'      
 
