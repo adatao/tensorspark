4992f0b2d5a42f5e503ab8ff9e57c19a129310ed
csmith <csmith@adatao.com>
2016-02-04 17:44:46 -0800

    Removed unnecessary serialization layers, json and base64.

--- parameter_server/parameterwebsocketclient.py
+++ parameter_server/parameterwebsocketclient.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/bpython
+# -*- coding: utf-8 -*-
 import json
 import tensorflow as tf
 import mnistdnn
@@ -7,7 +9,7 @@ import moleculardnn
 import math
 import numpy as np
 import tornado.websocket
-from tornado import gen
+from tornado import gen 
 from tornado.ioloop import IOLoop
 import cStringIO
 # TODO
@@ -54,80 +56,90 @@ class TensorSparkWorker(Borg):
       #elif model_keyword == 'molecular':
       #    self.model = moleculardnn.MolecularDNN(batch_size)
       #else:
-#         raise                                                                                                                                 
- #     IOLoop.current().run_sync(self.init_websocket)                                                                                           
- #     self.iteration = 0                                                                                                                       
-                                                                                                                                                
-   @gen.coroutine                                                                                                                               
-   def init_websocket(self):                                                                                                                    
-      self.websock = yield tornado.websocket.websocket_connect("ws://172.31.3.244:%d/" % self.websocket_port, connect_timeout=3600)             
-                                                                                                                                                
-   def train_partition(self, partition):                                                                                                        
-      while True:                                                                                                                               
-         #print 'TensorSparkWorker().train_partition iteration %d' % self.iteration                                                             
-         labels, features = self.model.process_partition(partition)                                                                             
-                                                                                                                                                
-         if len(labels) is 0:                                                                                                                   
-            break                                                                                                                               
-                                                                                                                                                
-         if self.time_to_pull(self.iteration):                                                                                                  
-                self.request_parameters()                                                                                                       
-                                                                                                                                                
-         self.model.train(labels, features)                                                                                                     
-         self.iteration += 1                                                                                                                    
-                                                                                                                                                
-         if self.time_to_push(self.iteration):                                                                                                  
-            self.push_gradients()                                                                                                               
-                                                                                                                                                
-      return []                                                                                                                                 
-      #return [self.train(x) for x in partition]                                                                                                
-                                                                                                                                                
-   def test_partition(self, partition):                                                                                                         
-      labels, features = self.model.process_partition(partition)                                                                                
-      self.request_parameters()                                                                                                                 
-      error_rate = self.model.test(labels, features)                                                                                            
-      return [error_rate]                                                                                                                       
-      #return [self.test(x) for x in partition]                                                                                                 
-                                                                                                                                                
-   def test(self, data):                                                                                                                        
-      #print 'TensorSparkWorker().test "%s"' % data                                                                                             
-      if len(data) is 0:                                                                                                                        
-         return 1.0                                                                                                                             
-      self.request_parameters()                                                                                                                 
-      accuracy = self.model.test(data)                                                                                                          
-      return accuracy                                                                                                                           
-#      self.model.                                                                                                                              
-                                                                                                                                                
-   def time_to_pull(self, iteration):                                                                                                           
-      return iteration % 5 == 0                                                                                                                 
-#      return True                                                                                                                              
-                                                                                                                                                
-   def time_to_push(self, iteration):                                                                                                           
-      return iteration % 5 == 0                                                                                                                 
-#      return True                                                                                                                              
-                                                                                                                                                
-   def request_parameters(self):                                                                                                                
-      IOLoop.current().run_sync(self.request_parameters_coroutine)                                                                              
-                                                                                                                                                
-   @gen.coroutine                                                                                                                               
-   def request_parameters_coroutine(self):                                                                                                      
-      request_model_message = {'type':'client_requests_parameters'}                                                                             
-      self.websock.write_message(json.dumps(request_model_message))                                                                             
-      #print 'requesting parameters'                                                                                                            
-      parameters = yield self.websock.read_message()                                                                                            
-      parameters = self.model.deserialize(parameters)                                                                                           
-      #parameters = pickle.loads(pickled_parameters)                                                                                            
-      #print 'received parameters'                                                                                                              
-      self.model.assign_parameters(parameters)                                                                                                  
-                                                                                                                                                
-   def push_gradients(self):                                                                                                                    
-      IOLoop.current().run_sync(self.push_gradients_coroutine)                                                                                  
-                                                                                                                                                
-   @gen.coroutine                                                                                                                               
-   def push_gradients_coroutine(self):                                                                                                          
-        #print 'pushing gradients'                                                                                                              
-        gradients = self.model.serialize(self.model.get_gradients())                                                                            
-        gradient_update_message = {'type':'client_gives_gradient', 'gradient':gradients}                                                        
-        self.websock.write_message(json.dumps(gradient_update_message))                                                                         
-        #print 'pushed gradients'                                                                                                               
-                                                                                                                                                
+#	  raise
+ #     IOLoop.current().run_sync(self.init_websocket)
+ #     self.iteration = 0
+
+   @gen.coroutine
+   def init_websocket(self):
+      self.websock = yield tornado.websocket.websocket_connect("ws://172.31.3.244:%d/" % self.websocket_port, connect_timeout=3600)
+
+   def train_partition(self, partition):
+      while True:
+         #print 'TensorSparkWorker().train_partition iteration %d' % self.iteration
+         labels, features = self.model.process_partition(partition)
+
+         if len(labels) is 0:
+            break
+
+         if self.time_to_pull(self.iteration):
+		self.request_parameters()
+
+         self.model.train(labels, features)
+         self.iteration += 1
+
+         if self.time_to_push(self.iteration):
+            self.push_gradients()
+
+      return []
+      #return [self.train(x) for x in partition]
+
+   def test_partition(self, partition):
+      labels, features = self.model.process_partition(partition)
+      self.request_parameters()
+      error_rate = self.model.test(labels, features)
+      return [error_rate]
+      #return [self.test(x) for x in partition]
+
+   def test(self, data):
+      #print 'TensorSparkWorker().test "%s"' % data
+      if len(data) is 0:
+         return 1.0
+      self.request_parameters()
+      accuracy = self.model.test(data)
+      return accuracy
+#      self.model.
+
+   def time_to_pull(self, iteration):
+      return iteration % 5 == 0
+#      return True
+
+   def time_to_push(self, iteration):
+      return iteration % 5 == 0
+#      return True
+
+   def request_parameters(self):
+      IOLoop.current().run_sync(self.request_parameters_coroutine)
+
+   @gen.coroutine
+   def request_parameters_coroutine(self):
+      # more like receive parameters now
+	parameters = yield self.websock.read_message()
+	parameters = self.model.deserialize(parameters)
+	self.model.assign_parameters(parameters)
+
+
+
+      #request_model_message = {'type':'client_requests_parameters'}
+      #self.websock.write_message(json.dumps(request_model_message))
+      #print 'requesting parameters'
+      #parameters = yield self.websock.read_message()
+      #parameters = self.model.deserialize(parameters)
+      #parameters = pickle.loads(pickled_parameters)
+      #print 'received parameters'
+      #self.model.assign_parameters(parameters)
+
+   def push_gradients(self):
+      IOLoop.current().run_sync(self.push_gradients_coroutine)
+
+   @gen.coroutine
+   def push_gradients_coroutine(self):
+      gradients = self.model.serialize(self.model.get_gradients())
+      self.websock.write_message(gradients, binary=True)
+
+      #print 'pushing gradients'
+      #gradients = self.model.serialize(self.model.get_gradients())
+      #gradient_update_message = {'type':'client_gives_gradient', 'gradient':gradients}
+      #self.websock.write_message(json.dumps(gradient_update_message))
+      #print 'pushed gradients'
+
