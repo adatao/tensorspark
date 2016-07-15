2d1ee5e06a6fd88c15f69272f3170b4a3dfc91dc
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-15 14:02:24 -0700

    create setup files for TSSpark package for Spark funcs on large scale Time Series & 3D Tensor data

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='TSSpark',
+    version='0.0.0',
+    packages=['TSSpark'],
+    url='https://github.com/adatao/tensorspark',
+    author='Arimo, Inc.',
+    author_email='info@arimo.com',
+    description='Spark functionalities for Time Series & 3D Tensor Data',
+    long_description='Spark functionalities for Time Series & 3D Tensor Data',
+    license='',
+    install_requires=[],
+    classifiers=[],
+    keywords='Spark Time Series Tensor TensorFlow')
